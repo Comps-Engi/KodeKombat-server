f867b1ccba172accabfeb564edae4c25cdc93940
Shashi Gowda <connect2shashi@gmail.com>
2011-10-05 03:11:23 +0530

    Models

--- Website/lib/models.php
+++ Website/lib/models.php
@@ -12,6 +12,10 @@ class User extends fActiveRecord {
         }
     }
 
+	function getMatches($since_id=null) {
+		return Match::getMatchesByUser($this->getId(), $since_id);
+	}
+
     function getScore() {
         return 0;
     }
@@ -22,23 +26,72 @@ class User extends fActiveRecord {
 }
 
 class Bot extends fActiveRecord {
+
+	function getUrl() {
+		return url('matches') . '?bot=' . $this->id;
+	}
 }
 
 class Match extends fActiveRecord {
 
-	static function getMatches($player_one_id, $player_two_id=null, $since=null, $offset=0, $limit=20) {
-		
+	public $winner;
+	public $loser;
+
+	function __construct() {
+		call_user_func_array(array('parent', '__construct'), func_get_args());
+		$this->winner = $this->getWinner();
+		$this->loser = $this->getLoser();
+	}
+
+	static function getMatches($since_id=null, $offset=0, $limit=20) {
+
+		$args = array();
+
+		if (!empty($since_id)) {
+			$args['id>'] = intval($since_id);
+		}
+
+		$matches = fRecordSet::build('Match', $args, $offset, $limit);
+
+		return $matches;
+	}
+
+	static function getMatchesByUser($user_id, $since_id=null, $limit=20) {
+
+		$args = array('player_one_id=|player_two_id=' => array($user_id, $user_id));
+
+		if (!empty($since_id)) {
+			$args['id>'] = intval($since_id);
+		}
+
+		$matches = fRecordSet::build('Match', $args, 0, $limit);
+
+		return $matches;
+	}
+
+	function getUrl() {
+		return url('match', array('id' => $this->getId()));
 	}
 
 	function getWinner() {
-		$winner_id = $this->getResult() == 0 ? $this->player_one_id : $this->player_two_id;
-		$winner = new User($winner_id);
+		$winner_id = $this->getResult() === 0 ? $this->getPlayerOneId() : $this->getPlayerTwoId();
+
+		try {
+			$winner = new User(intval($winner_id));
+		} catch (fNotFoundException $e) {
+			return null;
+		}
+
 		return $winner;
 	}
 
 	function getLoser() {
-		$loser_id = $this->getResult() == 1 ? $this->player_one_id : $this->player_two_id;
-		$loser = new User($loser_id);
+		$loser_id = $this->getResult() === 1 ? $this->player_one_id : $this->player_two_id;
+		try {
+			$loser = new User(intval($loser_id));
+		} catch (fNotFoundException $e) {
+			return null;
+		}
 		return $loser;
 	}
 
